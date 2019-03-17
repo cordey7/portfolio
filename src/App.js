1a0f0a73cd371b4757a31bf641e6d06126c2651f
Student <student@Admins-iMac.local>
2019-03-17 17:45:16 -0500

    new

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+    <title> Cordey Jones </title>
+    <h1>Programming Services</h1>
+</head>
+
+<body>
+    
+
+    <div class="nav button">
+        <input type="button" class="w3-btn w3-red" value="about">
+        <a class="btn-floating btn-large pulse">Contact</button>
+        <a href="#" class="w3-button w3-black">idk yet</a>
+    </div>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Fluid jumbotron</h1>
+          <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+      </div>
+
+
+export default App;
