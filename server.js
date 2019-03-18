377dc980e202d2d97e3deaebc2b70a6040ba6b63
Student <student@Admins-iMac.local>
2019-03-17 19:43:52 -0500

    serve

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-Parser')
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/contact-form', (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.name);
+});
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(3000);
